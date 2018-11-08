a6b5b4e4cf22907dc4c648d21eccbc554412176d
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2018-11-08 15:39:03 -0500

    successful login

--- crawler.py
+++ crawler.py
@@ -4,28 +4,47 @@ from http import *
 
 class Crawler:
     def __init__(self, host='fring.ccs.neu.edu'):
-        self.http = HTTP()
+        self.http = HTTP(host)
 
-    def login(self):
+    def login(self, username, password):
         get = HTTPRequest('GET', '/accounts/login/?next=/fakebook')
         response = self.http.send(get)
         print(str(get))
         print(response)
-        cookies = self.http.retrieve_cookies(response)
-        assert cookies is not None
-        self.csrf, self.session = cookies.split(";", 1)
-
-        print(self.csrf, self.session)
-        cookies= self.csrf +"; " + self.session
-        body = "username=001688440&password=GBLTTC6G&csrfmiddlewaretoken="+ self.csrf +"&next=%2Ffakebook%2F"
-
-        post = HTTPRequest('POST', '/accounts/login/?next=/fakebook', cookies=cookies, body=body)
+        assert self.retrieve_cookies(response) == True
+        body = "username="+username+"&password="+password+"&csrfmiddlewaretoken="+ self.csrf +"&next=%2Ffakebook%2F"
+        post = HTTPRequest('POST', '/accounts/login/?next=/fakebook', cookies=self.cookies, body=body)
         response = self.http.send(post)
         print(str(post))
         print(response)
+
+    # retrieve cookies
+    def retrieve_cookies(self, response):
+        num_cookies = response.count('Set-Cookie')
+        if (num_cookies != 2):
+            return False
+        current_start = 0
+        cookies = ""
+        counter = 0
+        while counter != 2:
+            set_start_index = response.find('Set-Cookie', current_start)
+            set_end_index = response.find('\r\n', set_start_index)
+            set_cookie = response[set_start_index : set_end_index]
+            cookie_start_index = 22
+            cookie_end_index = set_cookie.find(';')
+            cookie = set_cookie[cookie_start_index : cookie_end_index]
+            cookies += cookie + ";"
+            current_start = set_end_index
+            counter+=1
+        self.csrf, self.session = cookies[:-2].split(";", 1)
+        self.cookies = "csrftoken="+self.csrf+"; sessionid="+self.session
+        return True
+
+    def __del__(self):
+        self.http.__del__()
 
 
 
 
 crawler = Crawler()
-crawler.login()
+crawler.login("001688440", "GBLTTC6G")
--- request.py
+++ request.py
@@ -9,7 +9,7 @@ class HTTPRequest:
     # connection specifies whether to keep the connection alive
     # cookies is a dict of any cookies to include in the request
     # body is the body message
-    def __init__(self, method, resource, host='fring.ccs.neu.edu', cookies='', body='', version='1.0'):
+    def __init__(self, method, resource, host='fring.ccs.neu.edu', cookies='', body='', version='1.1'):
         self.method = method.upper()
         self.resource = resource
         self.version = version
@@ -25,8 +25,8 @@ class HTTPRequest:
             f'Host: {self.host}\r\n'
             f'Connection: keep-alive\r\n'
             f'Content-Length: {self.content_length}\r\n'
-            f'Cookie: {self.cookies}\r\n'
-            f'{self.body}\r\n\r\n'
+            f'Cookie: {self.cookies}\r\n\r\n'
+            f'{self.body}'
             )
         return request
 
